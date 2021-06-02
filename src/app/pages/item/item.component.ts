86c63bc2d2895523e5cb5ddc119b8f2a0be4b4c6
jose armando <jose4armando@outlook.com>
2021-06-02 14:19:14 -0500

    funcionalidad al next page

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -10,23 +10,45 @@ import { ProductInfo } from "../../interface/product-info.interface";
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-  product: ProductInfo = {};
-  productId: String = '';
-  isLoading = true;
+  product  : ProductInfo = {};
+  productId: string      = '';
+            nextPage     = { "url"  : "/home","label": "Ir al Inicio"};
+            isLoading    = true;
   constructor( private url: ActivatedRoute, private productService: ProductosService ) { }
 
-  ngOnInit(): void {
-
-    this.url.params.subscribe( params=>{
-      // console.log(params['id']);
-
-      this.productService.getProduct( params['id'] ).subscribe( (product: ProductInfo) =>{
-        this.product = product;
-        this.productId = params['id'];
-        this.isLoading = false;
+  ngOnInit(): void 
+  {
+    this.url.params.subscribe( params=>
+      {
+        this.productService.getProduct( params['id'] ).subscribe( (product: ProductInfo) =>
+        {
+          this.product   = product;
+          this.productId = params['id'];
+          this.isLoading = false;
+          if(product!==null )
+          {
+            this.isNextPage(this.productId);
+          }
+        });
       });
-    });
+  }
 
+  private isNextPage(id: string)
+  {
+    let nextPosibleProduct = "prod-"+( +( id.split("-")[1] ) + 1);
+    let elemento = this.productService.products.find(prod => prod.cod === nextPosibleProduct );
+
+    if(elemento)
+    {
+      this.nextPage.url   = "/item/"+elemento.cod;
+      this.nextPage.label = "Siguiente página";
+    }
+    else
+    {
+      this.nextPage.url   = "/home";
+      this.nextPage.label = "Ir al Inicio";
+    }
+    console.log("se ejecutó");
   }
 
 }
